a477be565bb6a39088c302c241ebe56a8d43a1da
DepressedProgrammer <125901616+DepressedProgrammer@users.noreply.github.com>
2025-08-10 19:25:49 +0000

    first commit

---
+++ media/lua/server/Items/distributeItems.lua
@@ -0,0 +1,12 @@
+require "Items/SuburbsDistributions"    
+require "Items/ProceduralDistributions"
+    -- table.insert(SuburbsDistributions["all"]["Fridge"].items, "base.Tiramisu")
+    -- table.insert(SuburbsDistributions["all"]["Fridge"].items, 90)
+    table.insert(SuburbsDistributions["all"]["bin"].items, "base.Tiramisu");
+    table.insert(SuburbsDistributions["all"]["bin"].items, 3);
+    table.insert(SuburbsDistributions["all"]["counter"].items, "base.Tiramisu");
+    table.insert(SuburbsDistributions["all"]["counter"].items,9);
+    table.insert(ProceduralDistributions["list"]["BakeryKitchenFridge"].items, "base.Tiramisu")
+    table.insert(ProceduralDistributions["list"]["BakeryKitchenFridge"].items,30)
+    table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, "base.Tiramisu")
+    table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items,30)
