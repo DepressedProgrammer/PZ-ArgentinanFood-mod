176b940709b29cac6e7b7f98c8781ce76a6b5cfb
DepressedProgrammer <companiazin@gmail.com>
2025-08-20 22:23:56 -0300

    Update distributeItems.lua

--- media/lua/server/Items/distributeItems.lua
+++ media/lua/server/Items/distributeItems.lua
@@ -1,7 +1,6 @@
 require "Items/SuburbsDistributions"    
 require "Items/ProceduralDistributions"
-    -- table.insert(SuburbsDistributions["all"]["Fridge"].items, "base.Tiramisu")
-    -- table.insert(SuburbsDistributions["all"]["Fridge"].items, 90)
+    --Tiramisu spawn
     table.insert(SuburbsDistributions["all"]["bin"].items, "base.Tiramisu");
     table.insert(SuburbsDistributions["all"]["bin"].items, 3);
     table.insert(SuburbsDistributions["all"]["counter"].items, "base.Tiramisu");
@@ -10,3 +9,56 @@ require "Items/ProceduralDistributions"
     table.insert(ProceduralDistributions["list"]["BakeryKitchenFridge"].items,30)
     table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, "base.Tiramisu")
     table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items,30)
+
+    --Chocolinas Spawn
+    table.insert(SuburbsDistributions["all"]["counter"].items, "base.Chocolinas");
+    table.insert(SuburbsDistributions["all"]["counter"].items,9);
+    table.insert(ProceduralDistributions["list"]["FoodGourmet"].items, "base.Chocolinas");
+    table.insert(ProceduralDistributions["list"]["FoodGourmet"].items, 30);
+    
+    table.insert(ProceduralDistributions["list"]["BakeryMisc"].items, "base.Chocolinas");
+    table.insert(ProceduralDistributions["list"]["BakeryMisc"].items, 30);
+    table.insert(ProceduralDistributions["list"]["ShelfGeneric"].items, "base.Chocolinas");
+    table.insert(ProceduralDistributions["list"]["ShelfGeneric"].items, 30);
+    table.insert(SuburbsDistributions["all"]["vendingsnack"].items, "base.Chocolinas");
+    table.insert(SuburbsDistributions["list"]["vendingsnack"].items, 30);
+
+    --Dulce de Leche Spawn
+    table.insert(SuburbsDistributions["all"]["counter"].items, "base.Dulce_De_Leche");
+    table.insert(SuburbsDistributions["all"]["counter"].items,9);
+    table.insert(ProceduralDistributions["list"]["FoodGourmet"].items, "base.Dulce_De_Leche");
+    table.insert(ProceduralDistributions["list"]["FoodGourmet"].items, 30);
+    
+    table.insert(ProceduralDistributions["list"]["BakeryMisc"].items, "base.Dulce_De_Leche");
+    table.insert(ProceduralDistributions["list"]["BakeryMisc"].items, 30);
+    table.insert(ProceduralDistributions["list"]["ShelfGeneric"].items, "base.Dulce_De_Leche");
+    table.insert(ProceduralDistributions["list"]["ShelfGeneric"].items, 30);
+    table.insert(SuburbsDistributions["all"]["vendingsnack"].items, "base.Dulce_De_Leche");
+    table.insert(SuburbsDistributions["list"]["vendingsnack"].items, 30);
+    --Chocotorta Spawn
+    table.insert(ProceduralDistributions["list"]["FoodGourmet"].items, "base.Chocotorta");
+    table.insert(ProceduralDistributions["list"]["FoodGourmet"].items, 30);
+    
+    table.insert(ProceduralDistributions["list"]["BakeryMisc"].items, "base.Chocotorta");
+    table.insert(ProceduralDistributions["list"]["BakeryMisc"].items, 30);
+    --Vanilla_Essence Spawn
+    table.insert(SuburbsDistributions["all"]["counter"].items, "base.Vanilla_Essence");
+    table.insert(SuburbsDistributions["all"]["counter"].items,9);
+    table.insert(ProceduralDistributions["list"]["FoodGourmet"].items, "base.Vanilla_Essence");
+    table.insert(ProceduralDistributions["list"]["FoodGourmet"].items, 30);
+    
+    table.insert(ProceduralDistributions["list"]["BakeryMisc"].items, "base.Vanilla_Essence");
+    table.insert(ProceduralDistributions["list"]["BakeryMisc"].items, 30);
+    table.insert(ProceduralDistributions["list"]["ShelfGeneric"].items, "base.Vanilla_Essence");
+    table.insert(ProceduralDistributions["list"]["ShelfGeneric"].items, 30);
+    --Cream spawn 
+    table.insert(SuburbsDistributions["all"]["counter"].items, "base.Cream");
+    table.insert(SuburbsDistributions["all"]["counter"].items,9);
+    table.insert(ProceduralDistributions["list"]["FoodGourmet"].items, "base.Vanilla_Essence");
+    table.insert(ProceduralDistributions["list"]["FoodGourmet"].items, 30);
+    
+    table.insert(ProceduralDistributions["list"]["BakeryMisc"].items, "base.Vanilla_Essence");
+    table.insert(ProceduralDistributions["list"]["BakeryMisc"].items, 30);
+    table.insert(ProceduralDistributions["list"]["ShelfGeneric"].items, "base.Vanilla_Essence");
+    table.insert(ProceduralDistributions["list"]["ShelfGeneric"].items, 30);
+
